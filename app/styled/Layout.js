306474e1d3c53dc4ce3a0f9f81514cac71b04716
Sam Slotsky <saxosamo@gmail.com>
2017-09-22 23:06:12 -0500

    styled-components and some text on the page

---
+++ app/styled/Layout.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const PageHeader = styled.header`
+  flex: 1 100%;
+  text-align: center;
+`;
+
+export const MainContent = styled.main`
+  flex: 1 100%;
+  font-size: 1.5rem;
+  padding: 2rem 5rem;
+`;
