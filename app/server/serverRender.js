76f4962e761e3093f9600b7650027ab23e785326
Sam Slotsky <saxosamo@gmail.com>
2017-10-13 21:54:11 -0500

    Fix 'straight to dashboard' bug

--- app/server/serverRender.js
+++ app/server/serverRender.js
@@ -4,12 +4,19 @@ import createStore from 'APP_ROOT/store'
 import { Provider } from 'react-redux'
 import { StaticRouter, matchPath } from 'react-router'
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
+import { initializeStore } from '@orange-marmalade/paginate-this';
 import App from 'APP_ROOT/App'
+import { setUser } from 'MODULES/identity/actions';
 import template from './template'
 import { bundle, vendorBundle } from './assets';
 
 export default function render(req, res) {
   const store = createStore();
+  initializeStore(store);
+
+  if (req.currentUser) {
+    store.dispatch(setUser(req.currentUser));
+  }
 
   const context = {};
 
